2eabd07bc6ab6c15cbe5eb17d8288ce49ae7a8e3
jvieirah <me.contrate.aero@gmail.com>
2023-03-13 21:23:38 -0300

    req2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -5,23 +5,25 @@ from parsel import Selector
 url = 'https://blog.betrybe.com/'
 # Requisito 1
 
+
 def fetch(url):
     """Seu código deve vir aqui"""
     time.sleep(1)
     try:
         headers = {'User-Agent': 'Fake user-agent'}
         web_page = requests.get(url, headers)
-    #A função deve retornar o conteúdo HTML da resposta.
         if web_page.status_code == 200:
             webtext = web_page.text
             return webtext
-    except ( requests.HTTPError, requests.ReadTimeout):
+    except (requests.HTTPError, requests.ReadTimeout):
         return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css('.entry-title > a::attr(href)').getall()
 
 
 # Requisito 3
