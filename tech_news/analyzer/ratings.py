df60700a953e395b13c11822ee9bd19c8e760e1d
jvieirah <me.contrate.aero@gmail.com>
2023-03-15 20:46:05 -0300

    dev-requirements.txt atualizado

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,19 @@
+from tech_news.database import get_collection
+
 # Requisito 10
 def top_5_categories():
     """Seu código deve vir aqui"""
+    news = get_collection()
+    categories = news.aggregate(
+        [
+            {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    response = []
+    for category in categories:
+        response.append(category["_id"])
+    return response
+
+
