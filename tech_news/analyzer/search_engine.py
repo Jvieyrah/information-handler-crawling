e9b46f45036ca3f978ce34104202ac007edf1cc8
jvieirah <me.contrate.aero@gmail.com>
2023-03-14 23:14:59 -0300

    exercicio 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    found_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    response = []
+    for news in found_news:
+        response.append((news["title"], news["url"]))
+    return response
+   
+    
+    
+
 
 
 # Requisito 8
